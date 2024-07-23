a7f9980d843089227afd7a4648271fa9e5588fae
Carl Gabriel <carldelapena2004@gmail.com>
2024-07-23 18:29:57 +1200

    removed useless code

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,8 +1,8 @@
 import styles from "./ContactCard.module.css";
-
 export default function ContactCard( {contact, onContactClicked} ) {
 
     function handleClick() {
+        //clicktester
         //console.log("Div clicked")
         onContactClicked(contact);
 
