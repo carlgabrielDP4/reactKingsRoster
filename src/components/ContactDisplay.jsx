0f1ac67144c272bcec6b81a55ac4e90ea1f7e33f
Carl Gabriel <carldelapena2004@gmail.com>
2024-07-23 22:36:53 +1200

    typing effect hook

--- src/components/ContactDisplay.jsx
+++ src/components/ContactDisplay.jsx
@@ -1,14 +1,18 @@
+import React from 'react';
+import { useTypingEffect } from '../hooks/useTypingEffect';
 import styles from "./ContactDisplay.module.css";
 
 export default function ContactDisplay({ contact }) {
+    const typedName = useTypingEffect(contact.name || '', 100);
+    const typedPhoneNumber = useTypingEffect(contact.phoneNumber || '', 50);
+    const typedFunFact = useTypingEffect(contact.funFact || '', 25);
 
     return (
         <main className={styles.container}>
             <img src={contact.photoUrl} alt={contact.name} />
-            <h1>{contact.name}</h1>
-            <h2>{contact.phoneNumber}</h2>
-            <p>{contact.funFact}</p>
+            <h1>{typedName}</h1>
+            <h2>{typedPhoneNumber}</h2>
+            <p>{typedFunFact}</p>
         </main>
     );
-
 }
---
+++ src/hooks/useTypingEffect.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+export const useTypingEffect = (text = '', speed = 200) => {
+  const [displayedText, setDisplayedText] = useState('');
+
+  useEffect(() => {
+    if (!text) {
+      setDisplayedText('');
+      return;
+    }
+
+    setDisplayedText('');
+    let currentIndex = 0;
+
+    const intervalId = setInterval(() => {
+      setDisplayedText((prev) => prev + text[currentIndex]);
+      currentIndex++;
+      if (currentIndex >= text.length-1) {
+        clearInterval(intervalId);
+      }
+    }, speed);
+
+    return () => {
+      clearInterval(intervalId); 
+    };
+  }, [text, speed]);
+
+  return displayedText;
+};
