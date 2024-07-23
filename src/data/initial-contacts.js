e351937a5c76a1611dfc3d080f52c298215f32bf
Carl Gabriel <carldelapena2004@gmail.com>
2024-07-23 16:31:42 +1200

    updated new player cards
    fixed lepookie

--- src/data/initial-contacts.js
+++ src/data/initial-contacts.js
@@ -10,9 +10,17 @@ export const INITIAL_CONTACTS = [
     _id: "5f90d372-7bdf-4b39-b621-cf6d3cd975f4",
     name: "Domantas Sabonis",
     phoneNumber: "PPG: 19.4 | APG: 8.2 | RPG: 13.7",
-    funFact: "A versatile big, equally adept at scoring and playmaking. Proud father of AD",
+    funFact: "Proud father of Anhony Davis and owns majority of land in Los Angeles.",
     photoUrl: "https://pbs.twimg.com/media/F_CHvibXEAA35nR.jpg"
   },
+  {
+    _id: "983c620f-6500-476f-a856-8791aac32794",
+    name: "Demar DeRozan",
+    phoneNumber: "PPG: 24.0 | APG: 4.3 | RPG: 5.3",
+    funFact: "Not Like Us.",
+    photoUrl: "https://nypost.com/wp-content/uploads/sites/2/2024/07/image_135f54.png?w=763"
+  },
+
   {
     _id: "b8853e3f-38d1-4712-9071-8f62d71c19b8",
     name: "Malik Monk",
@@ -20,13 +28,7 @@ export const INITIAL_CONTACTS = [
     funFact: "Explosive sparkplug who can jump out the building at any moment.",
     photoUrl: "https://res.cloudinary.com/ybmedia/image/upload/c_crop,h_1333,w_2000,x_0,y_0/c_fill,f_auto,h_1200,q_auto,w_1200/v1/m/3/f/3f65b9c3c945a31ef65f58fb3b9478a7386af02a/malik-monk-reveals-his-true-feelings-impending.jpg"
   },
-  {
-    _id: "983c620f-6500-476f-a856-8791aac32794",
-    name: "Davion Mitchell",
-    phoneNumber: "Lockdown Takeover",
-    funFact: "They call him off-night because they'll have an off-night",
-    photoUrl: "https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MjAyNTY5OTUwOTQwODk4MzE2/usatsi_21869825.jpg"
-  },
+
 
   {
     _id: "71b5ed07-9e1e-47d0-937a-64c396d5567b",
@@ -51,16 +53,16 @@ export const INITIAL_CONTACTS = [
   },
   {
     _id: "732c620f-8233-476f-99a2-8791aac32794",
-    name: "LeSunshine Pookie",
+    name: "LeSunshine",
     phoneNumber: "PPG: 55.3 APG: 62.4 RPG: 30.2",
     funFact: "Michael Jordan has no left hand, and played against art history majors in 97'",
-    photoUrl: "https://i.kym-cdn.com/entries/icons/original/000/049/004/lebronsunshinecover.jpg"
+    photoUrl: "https://files.vrchat.cloud/World-LeSunshine-Image-202236f1_.file_1113d5ce-1220-4e4a-87e7-c1ebd68ff1fe.1.png?Expires=1723680000&Key-Pair-Id=K3JAQ0Y971TV2Z&Signature=NJwgodQzxV3y0E13sOk3FXh2HaZgN8EFdAL~Y~uFx8D5Krul26lCrCG4rdV1FtEotVoWzjXIaK0L6TpQhfKGCAVIehQdOw8hM1rX0Rx1ifMJ7CCJqLJCZgNT5BpRPcJw-yj~oidEZzKiTBmhUqWrsjsM5E-RVLfuSgK64dRMqJe~bX0ZTd7plNhbhQNZaB0i4Fv6kYhaYCMuLH8PbHSyTlAQlsVNuUGPaYNkYh1HjF1vuTZ0EWH55jRIUZyf9GlJi5b8yKBQ9sS3teeEDOqOTWwbaCcZa3HCMyPcvxwbLIr5IvwmBiUPk-R0~mV0HBWJNhlfsEdOaqdauffSIl403Q__"
   },
   {
     _id: "abcd123-carl-476f-dela-8791pen32794",
     name: "Carl Dela Pena",
     phoneNumber: "NBA Commissioner",
     funFact: "Frontend > Backend.",
-    photoUrl: "https://avatars.githubusercontent.com/u/119389092?v=4"
+    photoUrl: "https://media.licdn.com/dms/image/D5603AQGI9WjCO6IOXg/profile-displayphoto-shrink_400_400/0/1713972451912?e=1727308800&v=beta&t=aA3Kr5dxNYEPBeemWv7fYlFD3aFKUmdjau8SgKfxuro"
   },
 ];
